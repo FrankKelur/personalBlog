759d012ac2851e56df3f143e090d5190e8da36f6
Zhai <frank@kelur.com>
2018-01-24 23:21:18 +0800

    add sql

---
+++ db/comment.sql
@@ -0,0 +1,5 @@
+INSERT INTO `comment` VALUES (1, 'Spring J Phoenix', '浅显易懂，Thumbs up！', '', 7);
+INSERT INTO `comment` VALUES (2, '百年目标第一季', '每次使用都比较好奇居然没有被墙。。。。', '', 7);
+INSERT INTO `comment` VALUES (3, 'baixiangcpp', '有一种小黄车就是基于 TOTP 算法的吧。4个按钮的那种小黄车。 ', '', 7);
+INSERT INTO `comment` VALUES (4, 'Spring J Phoenix', '浅显易懂，Thumbs up！', '', 7);
+INSERT INTO `comment` VALUES (5, '潞潞', '那个是固定的吧，出厂就不会变了。', '3', 7);
---
+++ db/recommend.sql
@@ -0,0 +1,3 @@
+INSERT INTO `recommend` VALUES (2, '/images/the-story-of-success.JPG', 'https://book.douban.com/subject/25863621/', '不一样的成功启示录', '怪才格拉德威尔告诉我们，如果没有机遇和文化、环境因素，即便是智商超过爱因斯坦，也只能做一份平庸的工作。');
+INSERT INTO `recommend` VALUES (3, '/images/the-story-of-success.JPG', 'https://book.douban.com/subject/25863621/', '不一样的成功启示录', '怪才格拉德威尔告诉我们，如果没有机遇和文化、环境因素，即便是智商超过爱因斯坦，也只能做一份平庸的工作。');
+INSERT INTO `recommend` VALUES (4, '/images/the-story-of-success.JPG', 'https://book.douban.com/subject/25863621/', '不一样的成功启示录', '怪才格拉德威尔告诉我们，如果没有机遇和文化、环境因素，即便是智商超过爱因斯坦，也只能做一份平庸的工作。');
